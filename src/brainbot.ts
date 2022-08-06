703a70585ab194e21d7d43d11fbb71b59e3b827d
XzFrosT <mschodeihsan@gmail.com>
2022-08-06 17:41:28 +0700

    added dotenv setup in bot file

--- src/brainbot.ts
+++ src/brainbot.ts
@@ -1,5 +1,10 @@
-import { Client } from "./base/Client";
+require('dotenv').config({
+	path: __dirname.substring(0, __dirname.lastIndexOf("/")) + "/.env"
+});
+require("./utils/error");
+
+import BrainBot from "./base/Client";
 import { connectToDatabase } from "./database";
 
 connectToDatabase();
-new Client().launch();
+(new BrainBot()).launch();
