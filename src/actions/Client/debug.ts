3170ba0f6a674a8b5e01df1657132e1ca01612ea
XzFrosT <mschodeihsan@gmail.com>
2022-08-24 14:07:59 +0700

    removed unnecessary event

--- src/actions/Client/debug.ts
+++
@@ -1,7 +0,0 @@
-import { WebhookClient } from 'discord.js'
-
-export default async (message: string, client: any) => {
-	if (process.env.NODE_ENV === "development") {
-		console.log(message);
-	}
-}
--- src/actions/Client/rateLimit.ts
+++
@@ -1,4 +0,0 @@
-export default (rateLimitData: any) => {
-	console.log(`Bot is being rate limited.`)
-	console.log(rateLimitData);
-}
