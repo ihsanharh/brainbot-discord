c9da4437d94b3ac6f057c15b74a8a83dc0538366
XzfrosT <mschodeihsan@gmail.com>
2024-12-17 12:36:37 +0700

    respond to discord webhook

--- server/src/routes/_interaction/index.ts
+++ server/src/routes/_interaction/index.ts
@@ -2,7 +2,6 @@ import { Request, Response, Router } from 'express';
 import { Worker } from 'node:worker_threads';
 import * as express from 'express';
 
-import { APIInteractionResponse } from 'discord-api-types/v10';
 import { HttpStatusCode } from "../../types/http";
 
 import { getAllApplicationCommands } from "../../managers/ApplicationCommand";
@@ -16,17 +15,21 @@ InteractionRoute.use(express.json({
 }));
 
 InteractionRoute.post("/", async (req: Request, res: Response) => {
+	res.status(HttpStatusCode.ACCEPTED).end();
+
+	inhandler(req.body);
+});
+
+async function inhandler(interaction: unknown): Promise<void> {
 	const collectors = await _active_collector();
 
-	const thread = new Worker(__dirname.replace("routes/_interaction", "interaction/handler"), {
+	new Worker(__dirname.replace("routes/_interaction", "interaction/handler"), {
 		workerData: {
 			collectors,
-			interaction: JSON.stringify(req.body),
+			interaction: JSON.stringify(interaction),
 			commands: JSON.stringify(await getAllApplicationCommands())
 		}
 	});
-	
-	thread.on('message', (message: APIInteractionResponse) => res.status(HttpStatusCode.OK).send(message));
-});
+}
 
 export { InteractionRoute };
